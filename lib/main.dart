49f024f65710a0e9663b58fc75c3bf4e12ec57e0
noa6277 <nfranck@hytrol.com>
2019-06-24 16:13:25 -0500

    Added the column widget & the image widget as well as a picture of food

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,11 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text("EasyList"),
         ),
+        body: Card(
+          child: Column(
+            children: <Widget>[Image(), Text("Food Paradise")],
+          ),
+        ),
       ),
     );
   }
